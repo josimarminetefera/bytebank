3806f7330de60bbcb92f1b28122e15a361404b4a
Josimar Minete <josimaminete@gmail.com>
2021-08-23 23:09:59 -0300

    Adicionando comando para emitir os dados do componente para o template.

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,11 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTranferir = new EventEmitter<any>();
+
   valor: number = 0;
   destino: number = 0;
+
   transferir() {
     console.log('NovaTransferenciaComponent');
     console.log(this.valor);
     console.log(this.destino);
+    const valorEmitir = {
+      valor: this.valor,
+      destino: this.destino,
+    }
+    this.aoTranferir.emit(valorEmitir);
   }
 }
