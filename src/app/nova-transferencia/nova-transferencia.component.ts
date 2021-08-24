ae1aae271535faec0b2624bcc1c8d99da66f4c46
Josimar Minete <josimaminete@gmail.com>
2021-08-23 21:26:19 -0300

    Adicionado primeiro componenet para adicionar nova transferencia.

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,13 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'nova-transferencia',
+    templateUrl:'.nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.componenet.scss']
+})
+
+
+export class NovaTransferenciaComponent{
+
+}
